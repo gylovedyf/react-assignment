1c98abb15933d320398ec2cc18c08ae62b7542c5
Yuan Gao <Yuan_Gao@comcast.com>
2018-07-06 21:20:05 -0400

    klkl

--- src/Components/App.js
+++ src/Components/App.js
@@ -42,7 +42,7 @@ class App extends Component {
 
     return (
       <div className="app">
-        <h3 className="header">This is in Testing Branch</h3>
+        <h3 className="header">This is in develop Branch</h3>
         <div>Diverge Testing</div>
         <div className='list-wrapper'>
           <h2>My List</h2>
